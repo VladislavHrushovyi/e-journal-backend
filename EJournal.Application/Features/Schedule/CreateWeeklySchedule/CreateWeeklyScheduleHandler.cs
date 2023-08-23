791c02625799ebe3912446423b61b4455af669c4
vladg <vlad.grush.99@gmail.com>
2023-08-23 12:19:29 +0300

    refactoring create weekly schedule

--- EJournal.Application/Features/Schedule/CreateWeeklySchedule/CreateWeeklyScheduleHandler.cs
+++ EJournal.Application/Features/Schedule/CreateWeeklySchedule/CreateWeeklyScheduleHandler.cs
@@ -19,44 +19,61 @@ public class CreateWeeklyScheduleHandler : IRequestHandler<CreateWeeklyScheduleR
     {
         var dateNow = DateTime.Now;
         var weeklySchedules = await _unitOfWork.WeeklyScheduleRepository.GetAll(cancellationToken);
-        var dateFirstDayOfWeek = DateOnly.FromDateTime(DateTime.Now)
-            .AddDays(-((int)DateTime.Today.DayOfWeek - 1));
+        Calendar cal = new CultureInfo("uk-UA").Calendar;
+        int week = cal.GetWeekOfYear(dateNow, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
 
         var schedulesList = weeklySchedules.ToList();
-        if ((
-                dateNow.DayOfWeek is DayOfWeek.Friday or DayOfWeek.Saturday or DayOfWeek.Sunday
-                && dateNow.Hour > 15
-                )
-            ||
-            !schedulesList.Any())
+        if (IsOldWeekSchedule(dateNow, schedulesList) || IsNotCurrWeek(week, schedulesList.Last()))
         {
-            if (schedulesList.Count > 1)
+            if (schedulesList.Count >= 1)
             {
-                await _unitOfWork.WeeklyScheduleRepository.Delete(schedulesList.Last(), cancellationToken);
+                schedulesList.ForEach(sc => _unitOfWork.WeeklyScheduleRepository.Delete(sc, cancellationToken));
             }
 
-            Calendar cal = new CultureInfo("uk-UA").Calendar;
-            int week = cal.GetWeekOfYear(dateNow, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
-            var allWorkingDays = await _unitOfWork._WorkDayRepository.GetAll(cancellationToken);
-            allWorkingDays = allWorkingDays.Select(workDay => new WorkDay()
-            {
-                Id = workDay.Id,
-                Date = dateFirstDayOfWeek.AddDays((int)workDay.DayOfWeek),
-                CreatedAt = workDay.CreatedAt,
-                DayOfWeek = workDay.DayOfWeek,
-                Times = workDay.Times,
-                UpdateAt = workDay.UpdateAt,
-                IsWorkDay = workDay.IsWorkDay
-            });
-            await _unitOfWork.WeeklyScheduleRepository.Create(new WeeklySchedule()
-            {
-                Id = Guid.NewGuid(),
-                CreatedAt = DateOnly.FromDateTime(dateNow),
-                WeekId = week + "-" + dateNow.Year,
-                WorkDays = allWorkingDays
-            }, cancellationToken);
+            await CreateNewWeeklySchedule(week, cancellationToken);
         }
 
         return new CreateWeeklyScheduleResponse();
+    }
+
+    private bool IsNotCurrWeek(int week, WeeklySchedule last)
+    {
+        var lastNumberScheduleWeekString = last.WeekId.Split("-")[0];
+
+        return week.ToString() != lastNumberScheduleWeekString;
+    }
+
+    private bool IsOldWeekSchedule(DateTime now, IEnumerable<WeeklySchedule> currWeeklySchedule)
+    {
+        return now.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday
+               ||
+               now.DayOfWeek is DayOfWeek.Friday && now.Hour >= 15
+               ||
+               !currWeeklySchedule.Any();
+    }
+
+    private async Task CreateNewWeeklySchedule(int currNumberWeek, CancellationToken cancellationToken)
+    {
+        var dateFirstDayOfWeek = DateOnly.FromDateTime(DateTime.Now)
+            .AddDays(-((int)DateTime.Today.DayOfWeek - 1));
+        
+        var allWorkingDays = await _unitOfWork._WorkDayRepository.GetAll(cancellationToken);
+        allWorkingDays = allWorkingDays.Select(workDay => new WorkDay()
+        {
+            Id = workDay.Id,
+            Date = dateFirstDayOfWeek.AddDays((int)workDay.DayOfWeek),
+            CreatedAt = workDay.CreatedAt,
+            DayOfWeek = workDay.DayOfWeek,
+            Times = workDay.Times,
+            UpdateAt = workDay.UpdateAt,
+            IsWorkDay = workDay.IsWorkDay
+        });
+        await _unitOfWork.WeeklyScheduleRepository.Create(new WeeklySchedule()
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = DateOnly.FromDateTime(DateTime.Now),
+            WeekId = currNumberWeek + "-" + dateFirstDayOfWeek.Year,
+            WorkDays = allWorkingDays
+        }, cancellationToken);
     }
 }
