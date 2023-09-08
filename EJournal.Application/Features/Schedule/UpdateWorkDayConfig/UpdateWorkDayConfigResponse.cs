0d46f5b43a7f5f928d48199d0ece4572a3327808
vladg <vlad.grush.99@gmail.com>
2023-09-08 11:35:03 +0300

    create structure for handling update workdays config

---
+++ EJournal.Application/Features/Schedule/UpdateWorkDayConfig/UpdateWorkDayConfigResponse.cs
@@ -0,0 +1,8 @@
+﻿using EJournal.Domain.Entities;
+
+namespace EJournal.Application.Features.Schedule.UpdateWorkDayConfig;
+
+public sealed class UpdateWorkDayConfigResponse
+{
+    public WorkDay WorkDay { get; set; }
+}
---
+++ EJournal.Application/Features/Schedule/UpdateWorkDayConfig/UpdateWorkDayConfigValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace EJournal.Application.Features.Schedule.UpdateWorkDayConfig;
+
+public class UpdateWorkDayConfigValidator : AbstractValidator<UpdateWorkDayConfigRequest>
+{
+    
+}
