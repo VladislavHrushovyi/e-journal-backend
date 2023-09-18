b609206c05fe82da22a3693861052ce7b25e449a
vladg <vlad.grush.99@gmail.com>
2023-09-18 15:21:28 +0300

    Test

--- EJournal.WebApi/Extension/Endpoints/ScheduleEndpoints.cs
+++ EJournal.WebApi/Extension/Endpoints/ScheduleEndpoints.cs
@@ -16,13 +16,15 @@ public static class ScheduleEndpoints
 {
     public static RouteGroupBuilder UseScheduleEndpoints(this RouteGroupBuilder group)
     {
-        group.MapPost("/add-work-day", [Authorize] async (AddWorkDayRequest req, IMediator mediatr, CancellationToken ct)
-            => await mediatr.Send(req, ct));
+        group.MapPost("/add-work-day", [Authorize]
+            async (AddWorkDayRequest req, IMediator mediatr, CancellationToken ct)
+                => await mediatr.Send(req, ct));
         group.MapPost("/add-time-to-work-day",
             [Authorize] async (AddTimeToWorkDayRequest req, IMediator mediator, CancellationToken ct)
                 => await mediator.Send(req, ct));
-        group.MapPost("/update-work-time-status", [Authorize] async (ChangeStatusRequest req, IMediator mediator, CancellationToken ct) 
-            => await mediator.Send(req, ct));
+        group.MapPost("/update-work-time-status", [Authorize]
+            async (ChangeStatusRequest req, IMediator mediator, CancellationToken ct)
+                => await mediator.Send(req, ct));
         group.MapGet("/actual-schedule", [Authorize] async (IMediator mediator, CancellationToken ct)
             => await mediator.Send(new GetActualWeeklyScheduleRequest(), ct));
         group.MapGet("/active-record", [Authorize] async (IMediator mediator, CancellationToken ct)
@@ -31,8 +33,9 @@ public static class ScheduleEndpoints
             => await mediator.Send(new GetWorkDaysRequest(), ct));
         group.MapPatch("/set-work-day", async (UpdateIsWorkDayRequest req, IMediator mediator, CancellationToken ct)
             => await mediator.Send(req, ct));
-        group.MapPatch("/delete-time", [Authorize] async ([FromBody]DeleteWorkTimeFromWorkDayRequest req, IMediator mediator, CancellationToken ct)
-            => await mediator.Send(req, ct));
+        group.MapPatch("/delete-time", [Authorize]
+            async ([FromBody] DeleteWorkTimeFromWorkDayRequest req, IMediator mediator, CancellationToken ct)
+                => await mediator.Send(req, ct));
         return group;
     }
 }
